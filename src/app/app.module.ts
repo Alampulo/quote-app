a205d34d3e610ee81d743867b672401c05d2bc9f
Bryan Juma <jumabryan10@gmail.com>
2018-08-10 17:14:26 +0300

    adds qoute-details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    QuoteDetailsComponent
   ],
   imports: [
     BrowserModule
