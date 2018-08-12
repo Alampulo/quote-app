c363842ea00652d4d84c07eae418e615675b31de
Bryan Juma <jumabryan10@gmail.com>
2018-08-12 20:13:38 +0300

    STYLES THE VIEW

--- src/app/form/app/quote.ts
+++
@@ -1,6 +0,0 @@
-export class Quote {
-  public showDetails:boolean;
-  constructor (public name:string, public quote:string, public author:string, public upVote: number, public downVote: number, public postingDate:Date,){
-    this.showDetails;false;
-  }
-}
