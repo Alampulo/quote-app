f9d8ff32c8793b91c6b4246e2bb4708a968c5d39
Bryan Juma <jumabryan10@gmail.com>
2018-08-12 19:24:32 +0300

    downvote added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
-import { QuoteDetailsComponent } from './quote-details/quote-details.component';
-import { QuoteFormComponent } from './quote-form/quote-form.component';
-import { StrikethroughDirective } from './strikethrough.directive';
-import { DateCountPipe } from './date-count.pipe';
+import { FormComponent } from './form/form.component';
 import { FormDetailsComponent } from './form-details/form-details.component';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    QuoteDetailsComponent,
-    QuoteFormComponent,
-    StrikethroughDirective,
-    DateCountPipe,
-    FormDetailsComponent
+    FormComponent,
+    FormDetailsComponent,
+    DateCountPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
