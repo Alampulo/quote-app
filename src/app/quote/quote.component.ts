e6a9ce6a365fef3ee4d7b9c6573f2c386eeb7ab4
Bryan Juma <jumabryan10@gmail.com>
2018-08-12 17:28:17 +0300

    adds delete quote function

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,26 +1,32 @@
-import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Quote } from '../quote';
 
 @Component({
-  selector: 'app-form-details',
-  templateUrl: './form-details.component.html',
-  styleUrls: ['./form-details.component.css']
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
 })
-export class FormDetailsComponent implements OnInit {
+export class QuoteComponent implements OnInit {
 
-  @Input() Quote:Quote;
-  @Output() complete = new EventEmitter<boolean>();
+  title='Quotes';
+  quotes = [
+    new Quote("", "", "", 0, 0, new Date())
+  ]
 
-  quoteDelete(deletequt:boolean) {
-    this.complete.emit(deletequt);
+  addNewQuote(quote) {
+    quote.postingDate = new Date();
+    this.quotes.push(quote);
+    console.log(this.quotes);
   }
 
-  upVote() {
-    this.Quote.upVote ++
+  deleteQuote(complete,index) {
+    if(complete) {
+      this.quotes.splice(index,1);
+    }
   }
 
-  downVote() {
-    this.Quote.downVote --
+  toggleDetails(index){
+   this.quotes[index].showDetails = !this.quotes[index].showDetails;
   }
 
   constructor() { }
