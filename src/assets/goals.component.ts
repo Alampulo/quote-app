571e862b58a9ea5a1f0af90d57666dcaf76da7ae
Bryan Juma <jumabryan10@gmail.com>
2018-08-16 09:27:12 +0300

    deletes background image

---
+++ src/assets/goals.component.ts
@@ -0,0 +1,15 @@
+// import { Component, OnInit } from '@angular/core';
+// import {Goals} from '../goals'
+// @Component({
+//   selector: 'app-goals',
+//   templateUrl: './goals.component.html',
+//   styleUrls: ['./goals.component.css']
+// })
+// export class GoalsComponent implements OnInit {
+//     goals = Goals;
+//   constructor() { }
+//
+//   ngOnInit() {
+//   }
+//
+// }
